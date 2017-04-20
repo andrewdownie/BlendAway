900503494f1f1d8cd2d0781bf3c5806a2b8a36d8
Andrew Downie <andrew_dev@outlook.com>
2017-04-20 04:25:15 +0000

    Base64 images are now saved to the file system again.

--- imageprocessing.php
+++ imageprocessing.php
@@ -30,7 +30,20 @@
     ///
     /// Convert Base64 images to filesystem images
     ///
+    function base64_to_jpeg($base64_string, $output_file) {
+        $ifp = fopen($output_file, "wb"); 
 
+        $data = explode(',', $base64_string);
+
+        fwrite($ifp, base64_decode($data[1])); 
+        fclose($ifp); 
+
+        return $output_file; 
+    }
+
+    for($i = 0; $i < 5; $i++){
+        base64_to_jpeg($images[$i], ($i + 1).".png");
+    }
 
 
 ?>
